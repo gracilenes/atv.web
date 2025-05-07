20cb84f07a32872cac7647735c16ed9cc61c8856
gracilenes <gracilenes876@gmail.com>
2025-05-07 19:44:12 +0000

    atv

--- flask/app.py
+++ flask/app.py
@@ -1,3 +1,20 @@
-from flask import *
+#importar a classe Flask
+from flask import Flask, render_template
+
+#instanciar o servidor Flask
 app = Flask(__name__)
+
+#criar variável global (lista de usuários)
 convidados = ["Gracilene"]
+
+#rota padrão (ir para página principal)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+#rota para direcionar para página de cadastro
+@app.route("/registre", methods=["GET", "POST"])
+def registre():
+    return render_template("cadastro.html")
+
+#rota para direcionar para página de lista de convidados
