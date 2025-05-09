b27d36837fca11db5c93d543e90d3d2bc3ca3bce
gracilenes <gracilenes876@gmail.com>
2025-05-09 16:51:34 +0000

    app

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,5 @@
 #importar a classe Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request, listaUsuarios
 
 #instanciar o servidor Flask
 app = Flask(__name__)
@@ -17,4 +17,36 @@ def index():
 def registre():
     return render_template("cadastro.html")
 
-#rota para direcionar para página de lista de convidados
+#rota para direcionar para página de cadastro
+@app.route("/paginaLista")
+def paginaCadastro():
+    return render_template("listar.html")
+
+@app.route("/cadastrarUsuario", methods=["POST"])
+def cadastrar():
+    
+    nome = request.form.get("nomeUsuario")
+    login = request.form.get("loginUsuario")
+    senha = request.form.get("senhaUsuario")
+    
+    listaUsuarios.append([nome, login, senha])
+    mensagem="Usuário Cadastrado com Sucesso"
+    return render_template("resultado.html", mensagem=mensagem)
+
+#criar uma rota para verificar se o usuário está na lista de convidados
+@app.route("/verificarUsuario", methods=['POST'])
+def verificar():
+    nome = request.form.get("nomeUsuario")
+    if(nome in convidados):
+        mensagem="você está convidado"
+        return render_template("resultado.html", mensagem=mensagem)
+    else:
+        mensagem="você NÃO está convidado"
+        return render_template("resultado.html", mensagem=mensagem)
+    
+@app.route("/listarConvidados")
+def listar():
+    return render_template("listar.html", convidados=convidados)
+
+#executar o servidor Flask
+app.run(debug=True)
